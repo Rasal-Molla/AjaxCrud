d1c7a702e20e6664b17606141233a2d1596719ce
Rasal-Molla <rasalmolla159401@gmail.com>
2023-05-26 20:52:20 +0600

    Products table

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('products');
-});
+Route::get('/', [ProductController::class, 'index'])->name('product.index');
